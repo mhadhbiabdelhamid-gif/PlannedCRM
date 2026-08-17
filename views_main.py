2a85dd2ca9c768af6b663130aae0f971ab007c63
Mhadhbi Abdelhamid <mhadhbi.abdelhamid@gmail.com>
2026-08-17 19:02:23 +0300

    Redirect root to dashboard

--- views_main.py
+++ views_main.py
@@ -13,7 +13,7 @@ bp = Blueprint("main", __name__)
 @bp.route('/')
 def index():
     # This tells the server what to show when visiting the main web address
-    return render_template('index.html')
+    return redirect(url_for('main.dashboard'))
 
 @bp.route("/")
 @login_required
